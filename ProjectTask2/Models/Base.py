d34ca3d205e5b4206c805a42e868ef38522d78ae
Prizrak726 <ds381689@gmail.com>
2026-01-23 15:52:28 +0500

    Add files via upload

---
+++ ProjectTask2/Models/Base.py
@@ -0,0 +1,6 @@
+from Connection.connect import *
+
+class BaseModel(Model):
+    class Meta:
+        database = mysql_db
+
---
+++ ProjectTask2/Views/SearchView.py
@@ -0,0 +1,10 @@
+from tkinter import *
+from tkinter import ttk
+
+from Controllers.UserControllers import UserController
+
+class SearchView(Tk):
+    def __init__(self, search_string):
+        super().__init__()
+
+
