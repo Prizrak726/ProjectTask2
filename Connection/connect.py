b4c05742c6bafa9648a618bb43acb2721f49a2c2
Prizrak726 <ds381689@gmail.com>
2026-01-27 10:10:26 +0500

    Add files via upload

---
+++ Connection/connect.py
@@ -0,0 +1,13 @@
+from peewee import *
+
+# Connect to a MySQL database on network.
+mysql_db = MySQLDatabase('SereD62_Task2',
+                         user='SereD62_Task2',
+                         password='111111',
+                         host='10.11.13.118',
+                         port=3306)
+if __name__ == '__main__':
+
+
+    print(mysql_db.connect())
+
